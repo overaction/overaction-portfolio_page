316e2d54ea869d3ba8a5696a475ac3a4909f5292
mckim <kmc04@naver.com>
2022-07-27 22:57:38 +0900

    Navbar 적용

---
+++ portfolio/components/Navbar.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import {
+    AiOutlineClose,
+    AiOutlineMenu,
+    AiFillGithub,
+    AiOutlineBold,
+    AiOutlineMail,
+} from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(true)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+    return (
+        <div className="fixed w-full h-20 shadow-xl z-[100]">
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+                <Image
+                    src="/../public/assets/logoImg.png"
+                    alt="/"
+                    width="125"
+                    height="70"
+                />
+                <div>
+                    <ul className="hidden md:flex">
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b">
+                                home
+                            </li>
+                        </Link>
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b">
+                                About
+                            </li>
+                        </Link>
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b">
+                                Skills
+                            </li>
+                        </Link>
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b">
+                                Projects
+                            </li>
+                        </Link>
+                        <Link href="/">
+                            <li className="ml-10 text-sm uppercase hover:border-b">
+                                Contact
+                            </li>
+                        </Link>
+                    </ul>
+                    <div onClick={handleNav} className="md:hidden">
+                        <AiOutlineMenu size={25} />
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className={
+                    nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''
+                }
+            >
+                <div
+                    className={
+                        nav
+                            ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                            : 'fixed left-[-100%] top-0 ease-in duration-500'
+                    }
+                >
+                    <div>
+                        <div className="flex w-full items-center justify-between">
+                            <Image
+                                src="/../public/assets/logoImg.png"
+                                width="80"
+                                height="50"
+                                alt="/"
+                            />
+                            <div
+                                onClick={handleNav}
+                                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
+                            >
+                                <AiOutlineClose />
+                            </div>
+                        </div>
+                        <div className="border-b border-gray-300 my-4">
+                            <p className="w-[85%] md:w-[90%] py-4">gogo</p>
+                        </div>
+                    </div>
+                    <div className="py-4 flex flex-col">
+                        <ul>
+                            <Link href="/">
+                                <li className="py-4 text-sm">Home</li>
+                            </Link>
+                            <Link href="/">
+                                <li className="py-4 text-sm">About</li>
+                            </Link>
+                            <Link href="/">
+                                <li className="py-4 text-sm">Skills</li>
+                            </Link>
+                            <Link href="/">
+                                <li className="py-4 text-sm">Projects</li>
+                            </Link>
+                            <Link href="/">
+                                <li className="py-4 text-sm">Contact</li>
+                            </Link>
+                        </ul>
+                        <div className="pt-40">
+                            <p className="uppercase tracking-widest text-[#5651e5]">
+                                Let's Connect
+                            </p>
+                            <div className="flex items-center my-4 w-full sm:w-[80%]">
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 mr-4 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiFillGithub />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 mr-4 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiOutlineBold />
+                                </div>
+                                <div className="rounded-full shadow-lg shadow-gray-400 p-3 mr-4 cursor-pointer hover:scale-105 ease-in duration-300">
+                                    <AiOutlineMail />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
