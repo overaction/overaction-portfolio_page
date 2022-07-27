e5948c8fe00000fe86eb6bc3245c235456ab66f9
mckim <kmc04@naver.com>
2022-07-28 02:14:52 +0900

    About 페이지 제작

---
+++ portfolio/components/About.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { CgProfile } from 'react-icons/cg'
+import {BsFillCalendarFill} from 'react-icons/bs'
+import {IoMdCall} from 'react-icons/io'
+import {
+    AiOutlineMail,
+} from 'react-icons/ai'
+const About = () => {
+    return (
+        <div className="w-full h-screen p-2 flex items-center py-16 text-center">
+                <div className="w-full flex justify-center items-center flex-col">
+                    <h1 className="inline-block border-b-4 border-gray-300 py-2 mb-12">
+                        About Me
+                    </h1>
+                    <ul className="flex flex-wrap justify-center items-center flex-col sm:flex-row ml-20 gap-8">
+                        <li className='flex w-[100%] flex-[40%]'>
+                            <div>
+                                <CgProfile size={25}/>
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='text-lg font-bold'>이름</h1>
+                                <div className='text-lg font-semibold text-gray-700'>김만철</div>
+                            </div>
+                        </li>
+                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                            <div>
+                                <BsFillCalendarFill size={25} />
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='text-lg font-bold'>생년월일</h1>
+                                <div className='text-lg font-semibold text-gray-700'>98.05.20</div>
+                            </div>
+                        </li>
+                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                            <div>
+                                <IoMdCall size={25} />
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='text-left text-lg font-bold'>연락처</h1>
+                                <div className='text-lg font-semibold text-gray-700'>010-3263-3921</div>
+                            </div>
+                        </li>
+                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                            <div>
+                                <AiOutlineMail size={25} />
+                            </div>
+                            <div className='ml-4'>
+                                <h1 className='text-left text-lg font-bold'>이메일</h1>
+                                <div className='text-lg font-semibold text-gray-700'>kmc0520kmc@gmail.com</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    )
+}
+
+export default About
