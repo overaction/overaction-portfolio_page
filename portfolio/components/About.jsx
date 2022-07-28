b9870a348360795e34b5b191602db8e067005b4d
mckim <kmc04@naver.com>
2022-07-28 15:37:26 +0900

    About 페이지 css 수정

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -7,13 +7,13 @@ import {
 } from 'react-icons/ai'
 const About = () => {
     return (
-        <div className="w-full h-screen p-2 flex items-center py-16 text-center">
+        <div className="w-full  p-2 flex items-center text-center">
                 <div className="w-full flex justify-center items-center flex-col">
                     <h1 className="inline-block border-b-4 border-gray-300 py-2 mb-12">
                         About Me
                     </h1>
-                    <ul className="flex flex-wrap justify-center items-center flex-col sm:flex-row ml-20 gap-8">
-                        <li className='flex w-[100%] flex-[40%]'>
+                    <ul className="flex flex-wrap justify-center items-center flex-col sm:flex-row  gap-8">
+                        <li className='flex w-[100%] sm:w-8 flex-[40%] mt-3 pl-14 pb-8'>
                             <div>
                                 <CgProfile size={25}/>
                             </div>
@@ -22,7 +22,7 @@ const About = () => {
                                 <div className='text-lg font-semibold text-gray-700'>김만철</div>
                             </div>
                         </li>
-                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                        <li className='flex w-[100%] sm:w-8 flex-[40%] mt-3 pl-14 pb-8'>
                             <div>
                                 <BsFillCalendarFill size={25} />
                             </div>
@@ -31,7 +31,7 @@ const About = () => {
                                 <div className='text-lg font-semibold text-gray-700'>98.05.20</div>
                             </div>
                         </li>
-                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                        <li className='flex w-[100%] flex-[40%] mt-3 pl-14 pb-8'>
                             <div>
                                 <IoMdCall size={25} />
                             </div>
@@ -40,7 +40,7 @@ const About = () => {
                                 <div className='text-lg font-semibold text-gray-700'>010-3263-3921</div>
                             </div>
                         </li>
-                        <li className='flex w-[100%] flex-[40%] mt-3'>
+                        <li className='flex w-[100%] flex-[40%] mt-3 pl-14 pb-8'>
                             <div>
                                 <AiOutlineMail size={25} />
                             </div>
